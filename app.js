36f98a70552a0fa66a65f5c50846832243009595
akramhelil <akramhelil@gmail.com>
2019-08-15 11:31:33 -0400

    added the post route

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const PORT = 3000;
+
+
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+
 
 
 app.get('/', (req, res) => {
@@ -14,8 +20,17 @@ app.get('/campgrounds', (req, res) => {
         {name: "Central Park", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkm74HV67ZUNWNNnA5HpqfrfLcyUrcZGA2n0NYJH0VbKgBP5rFcA"},
         {name: "Prospect Park", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkm74HV67ZUNWNNnA5HpqfrfLcyUrcZGA2n0NYJH0VbKgBP5rFcA"},
     ]
-    res.render('campgrounds');
+    res.render('campgrounds', {campgrounds: campgrounds});
 
 });
+// make the route to the convintion as REST restful toutes 7 convention
+app.post('/campgrounds', (req, res) => {
+    // get data from the form and add to the campgroudns
+    // rediret to the camgrounds
+});
 
-app.listen(3000, console.log('Server is Started'))
+app.get('/camgrounds/new', (res, req) => {
+    res.render('new')
+});
+
+app.listen(PORT, console.log('Server is Started on Port', PORT))
