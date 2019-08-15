745c29f0449cf6202a763d754440fd85667521f0
akramhelil <akramhelil@gmail.com>
2019-08-15 10:53:35 -0400

    campground route setup

--- app.js
+++ app.js
@@ -4,13 +4,18 @@ app.set('view engine', 'ejs');
 
 
 app.get('/', (req, res) => {
-    res.render('landing')
+    res.render('landing');
 });
 
 app.get('/campgrounds', (req, res) => {
-    let campgrounds = []
-    // res.send('Hello')
+    let campgrounds = [
+        {name: "MC Park", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkm74HV67ZUNWNNnA5HpqfrfLcyUrcZGA2n0NYJH0VbKgBP5rFcA"},
+        {name: "Central Park", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkm74HV67ZUNWNNnA5HpqfrfLcyUrcZGA2n0NYJH0VbKgBP5rFcA"},
+        {name: "Central Park", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkm74HV67ZUNWNNnA5HpqfrfLcyUrcZGA2n0NYJH0VbKgBP5rFcA"},
+        {name: "Prospect Park", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkm74HV67ZUNWNNnA5HpqfrfLcyUrcZGA2n0NYJH0VbKgBP5rFcA"},
+    ]
+    res.render('campgrounds');
 
 });
 
-app.listen(3000, console.log('Server is Started'));
+app.listen(3000, console.log('Server is Started'))
