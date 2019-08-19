c64360e5344078d72e058dc8c9ba2c19786dc030
akramhelil <akramhelil@gmail.com>
2019-08-19 16:21:40 -0400

    small changes on the APO

--- app.js
+++ app.js
@@ -8,28 +8,38 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
+let campgrounds = [
+    {name: "MC Park", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkm74HV67ZUNWNNnA5HpqfrfLcyUrcZGA2n0NYJH0VbKgBP5rFcA"},
+    {name: "Central Park", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkm74HV67ZUNWNNnA5HpqfrfLcyUrcZGA2n0NYJH0VbKgBP5rFcA"},
+    {name: "Central Park", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkm74HV67ZUNWNNnA5HpqfrfLcyUrcZGA2n0NYJH0VbKgBP5rFcA"},
+    {name: "Prospect Park", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkm74HV67ZUNWNNnA5HpqfrfLcyUrcZGA2n0NYJH0VbKgBP5rFcA"},
+]
 
 app.get('/', (req, res) => {
     res.render('landing');
 });
 
 app.get('/campgrounds', (req, res) => {
-    let campgrounds = [
-        {name: "MC Park", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkm74HV67ZUNWNNnA5HpqfrfLcyUrcZGA2n0NYJH0VbKgBP5rFcA"},
-        {name: "Central Park", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkm74HV67ZUNWNNnA5HpqfrfLcyUrcZGA2n0NYJH0VbKgBP5rFcA"},
-        {name: "Central Park", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkm74HV67ZUNWNNnA5HpqfrfLcyUrcZGA2n0NYJH0VbKgBP5rFcA"},
-        {name: "Prospect Park", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkm74HV67ZUNWNNnA5HpqfrfLcyUrcZGA2n0NYJH0VbKgBP5rFcA"},
-    ]
     res.render('campgrounds', {campgrounds: campgrounds});
 
 });
 // make the route to the convintion as REST restful toutes 7 convention
 app.post('/campgrounds', (req, res) => {
+    
     // get data from the form and add to the campgroudns
+    let name = req.body.name
+    let image = req.body.image
+    let newCampGround = {
+        name: name,
+        image: image
+    }
+    campgrounds.push(newCampGround)
     // rediret to the camgrounds
-    console.log('POSTED')
+   res.redirect('/campgrounds')
 });
 
+
+// Create Form Render 
 app.get('/campgrounds/new', (req, res) => {
     res.render('new');
 });
