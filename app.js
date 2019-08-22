e0be4b517e58b207e073b066e67e08d3bb89308d
akramhelil <akramhelil@gmail.com>
2019-08-22 14:58:32 -0400

    connected mongoose to the NodeJS

--- app.js
+++ app.js
@@ -2,29 +2,46 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const PORT = 3000;
-
+const mongoose = require('mongoose')
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
+// connect mongoose to our local database
+mongoose.connect('mongodb://localhost/yelpcamp', { useNewUrlParser: true });
+// setup schema 
+const campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
+
+const Campground = mongoose.model('Campground', campgroundSchema)
+
+// Campground.create({
+//     name: "MC Park",
+//     image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkm74HV67ZUNWNNnA5HpqfrfLcyUrcZGA2n0NYJH0VbKgBP5rFcA"
+// }, (err, newCampGround) => {
+//         if (err) {
+//             console.log("SOmething Went Wrong Try again", err)
+//         } else {
+//             console.log("New Camp Ground is created", newCampGround )
+//         }
+// } )
 
 
-let campgrounds = [
-    {name: "MC Park", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkm74HV67ZUNWNNnA5HpqfrfLcyUrcZGA2n0NYJH0VbKgBP5rFcA"},
-    {name: "Central Park", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkm74HV67ZUNWNNnA5HpqfrfLcyUrcZGA2n0NYJH0VbKgBP5rFcA"},
-    {name: "Central Park", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkm74HV67ZUNWNNnA5HpqfrfLcyUrcZGA2n0NYJH0VbKgBP5rFcA"},
-    {name: "Central Park", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkm74HV67ZUNWNNnA5HpqfrfLcyUrcZGA2n0NYJH0VbKgBP5rFcA"},
-    {name: "Central Park", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkm74HV67ZUNWNNnA5HpqfrfLcyUrcZGA2n0NYJH0VbKgBP5rFcA"},
-    {name: "Central Park", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkm74HV67ZUNWNNnA5HpqfrfLcyUrcZGA2n0NYJH0VbKgBP5rFcA"},
-    {name: "Central Park", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkm74HV67ZUNWNNnA5HpqfrfLcyUrcZGA2n0NYJH0VbKgBP5rFcA"},
-    {name: "Prospect Park", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkm74HV67ZUNWNNnA5HpqfrfLcyUrcZGA2n0NYJH0VbKgBP5rFcA"},
-]
 
 app.get('/', (req, res) => {
     res.render('landing');
 });
 
 app.get('/campgrounds', (req, res) => {
-    res.render('campgrounds', {campgrounds: campgrounds});
+    Campground.find({}, (err, allCampgrounds) => {
+        if (err) {
+            console.log('SOmething went wrong', err)
+        } else {
+            
+            res.render('campgrounds', {campgrounds: allCampgrounds});
+        }
+    })
 });
 // make the route to the convintion as REST restful toutes 7 convention
 app.post('/campgrounds', (req, res) => {
@@ -36,9 +53,14 @@ app.post('/campgrounds', (req, res) => {
         name: name,
         image: image
     }
-    campgrounds.push(newCampGround)
-    // rediret to the camgrounds
-   res.redirect('/campgrounds')
+    Campground.create(newCampGround, (err, newCampgtound) => {
+        if (err) {
+            console.log("SOmethign went wrong", err)
+        } else {
+            // rediret to the camgrounds
+            res.redirect('/campgrounds')
+        }
+    }); 
 });
 
 
